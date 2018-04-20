a9181887951cd79e73efbd6fdc5cd51de4a480fc
Andrew Stanley <andystanley@hotmail.ca>
2018-04-20 15:43:23 -0400

    Updated to recieve and process stats from StatsInput
    API calls not yet implemented

--- nba-stat-tracker/src/screens/topPerformers.js
+++ nba-stat-tracker/src/screens/topPerformers.js
@@ -1,11 +1,77 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import axios from 'axios';
 
 export default class TopPerformers extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      points: 0,
+      assists: 0,
+      rebounds: 0,
+      img1: null,
+      img2: null,
+      img3: null
+    };
+  }
+
+  componentDidMount() {
+    const { stats } = this.props.navigation.state.params;
+
+    let pointsPlayer = '';
+    let assistsPlayer = '';
+    let reboundsPlayer = '';
+
+    for (i=0; i < stats.length; i++) {
+      if (stats[i].points > this.state.points) {
+        this.setState({points: stats[i].points});
+        let name = stats[i].name;
+        name = name.split(' ').slice(-1).join(' ');
+        pointsPlayer = name;
+      }
+
+      if (stats[i].assists > this.state.assists) {
+        this.setState({assists: stats[i].assists});
+        let name = stats[i].name;
+        name = name.split(' ').slice(-1).join(' ');
+        assistsPlayer = name;
+      }
+
+      if (stats[i].rebounds > this.state.rebounds) {
+        this.setState({rebounds: stats[i].rebounds});
+        let name = stats[i].name;
+        name = name.split(' ').slice(-1).join(' ');
+        reboundsPlayer = name;
+      }
+    }
+
+    // Not currently functional
+    axios.get('https://nba-players.herokuapp.com/players/' + pointsPlayer)
+    .then(res => {
+      const img1 = res.data;
+      this.setState({ img1 });
+    });
+
+    axios.get('https://nba-players.herokuapp.com/players/' + assistsPlayer)
+    .then(res => {
+      const img2 = res.data;
+      this.setState({ img2 });
+    });
+
+    axios.get('https://nba-players.herokuapp.com/players/' + reboundsPlayer)
+    .then(res => {
+      const img3 = res.data;
+      this.setState({ img3 });
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>Top Performers</Text>
+        <Text>{this.state.points}</Text>
+        <Text>{this.state.assists}</Text>
+        <Text>{this.state.rebounds}</Text>
       </View>
     );
   }
